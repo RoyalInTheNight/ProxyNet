a8efc105edff40bd0fcc26a525f52814e3223693
RITN <rointhenight@gmail.com>
2023-10-11 15:55:10 +0000

    sha imp

--- include/crypto/sha256.h
+++ include/crypto/sha256.h
@@ -1,7 +1,32 @@
 #ifndef SHA256_CRYPTO
 #define SHA256_CRYPTO
 
+#include <cstdint>
 #include <string>
+
+#define SHA2_SHFR(x, n)    (x >> n)
+#define SHA2_ROTR(x, n)   ((x >> n) | (x << ((sizeof(x) << 3) - n)))
+#define SHA2_ROTL(x, n)   ((x << n) | (x >> ((sizeof(x) << 3) - n)))
+#define SHA2_CH(x, y, z)  ((x & y) ^ (~x & z))
+#define SHA2_MAJ(x, y, z) ((x & y) ^ (x & z) ^ (y & z))
+#define SHA256_F1(x) (SHA2_ROTR(x,  2) ^ SHA2_ROTR(x, 13) ^ SHA2_ROTR(x, 22))
+#define SHA256_F2(x) (SHA2_ROTR(x,  6) ^ SHA2_ROTR(x, 11) ^ SHA2_ROTR(x, 25))
+#define SHA256_F3(x) (SHA2_ROTR(x,  7) ^ SHA2_ROTR(x, 18) ^ SHA2_SHFR(x,  3))
+#define SHA256_F4(x) (SHA2_ROTR(x, 17) ^ SHA2_ROTR(x, 19) ^ SHA2_SHFR(x, 10))
+#define SHA2_UNPACK32(x, str)                 \
+{                                             \
+    *((str) + 3) = (uint8_t) ((x)      );       \
+    *((str) + 2) = (uint8_t) ((x) >>  8);       \
+    *((str) + 1) = (uint8_t) ((x) >> 16);       \
+    *((str) + 0) = (uint8_t) ((x) >> 24);       \
+}
+#define SHA2_PACK32(str, x)                   \
+{                                             \
+    *(x) =   ((uint32_t) *((str) + 3)      )    \
+           | ((uint32_t) *((str) + 2) <<  8)    \
+           | ((uint32_t) *((str) + 1) << 16)    \
+           | ((uint32_t) *((str) + 0) << 24);   \
+}
 
 namespace sha {
     const unsigned int sha_K[] = {
@@ -15,20 +40,24 @@ namespace sha {
         0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
     };
 
-    const unsigned init_hash_value[] = {
+    unsigned int m_h[] = {
         0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
     };
 }
 
 class sha256 {
 private:
-    static int ROTR(const int, const int);
-    static int math_sigma1(const int);
-    static int math_sigma2(const int);
-    static int math1(const int);
-    static int math2(const int);
-    static int choice(const int, const int, const int);
-    static int majority(const int, const int, const int);
+    void transform(const uint8_t *, uint32_t);
+    void update(const uint8_t *, uint32_t);
+    void final(uint8_t *);
+
+    unsigned int m_tot_len;
+    unsigned int m_len;
+
+    static const unsigned int SHA224_256_BLOCK_SIZE = (512 / 8);
+    static const unsigned int DIGEST_SIZE           = (256 / 8);
+
+    unsigned char m_block[2 * SHA224_256_BLOCK_SIZE];
 
 public:
     enum class sha256_options
@@ -37,6 +66,7 @@ public:
         option_hex_hash    = 0xfffffff 
     };
 
+    sha256();
     static std::string hash(const std::string&, const sha256_options&);
 };
 
